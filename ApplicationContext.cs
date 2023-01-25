a0ad3f7d00b5639e5024cfb64adecb501a3523dc
Dmitry Grigorev <d-d12@mail.ru>
2023-01-25 20:49:08 +0400

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LINQEF
+{
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+
+
+}
---
+++ MyEntities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQEF
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public List<User> Users { get; set; } = new();
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company? Company { get; set; }
+    }
+
+}
